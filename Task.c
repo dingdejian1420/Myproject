9c4013a7c8f1bb8385d9b6ddd5fbcdc34eb23531
dingdejian1420 <dingjian3399@sina.com>
2019-11-07 18:23:54 +0800

    Update Task.c

--- Task.c
+++ Task.c
@@ -37,3 +37,55 @@ void Task_vClrRdyTable(uint8 u8Prio)
 
 uint8 Task_GetHighestPrioIndex(void)
 {
+	uint8 u8First1Index;
+	if(Task_u32RdyTable&0xFF)
+	{
+		u8First1Index=First1MapTbl[Task_u32RdyTable&0xFF];
+	}
+	else if(Task_u32RdyTable&0xFF00)
+	{
+		u8First1Index=First1MapTbl[(Task_u32RdyTable&0xFF00)>>8]+7;
+	}
+	else if(Task_u32RdyTable&0xFF0000)
+	{
+		u8First1Index=First1MapTbl[(Task_u32RdyTable&0xFF0000)>>16]+7+8;
+	}
+	else if(Task_u32RdyTable&0xFF000000)
+	{
+		u8First1Index=First1MapTbl[(Task_u32RdyTable&0xFF000000)>>24]+7+8+8;
+	}
+	else
+	{
+		return 0xFF;//error
+	}
+		
+	return u8First1Index;
+}
+
+void Task_vInit(tstTaskBlock *pstTask, void *pvStackPointer, tfEnteryFun fEnterFun, uint8 u8Prio)
+{
+	pstTask->fEnterFun=fEnterFun;
+	pstTask->pStackPointer=pvStackPointer;
+	pstTask->u32TaskTick=0;
+	pstTask->u8Prio=u8Prio;
+	
+	pstTask->stTaskDelayLink.pstNext=&pstTask->stTaskDelayLink;
+	pstTask->stTaskDelayLink.pstPre=&pstTask->stTaskDelayLink;
+	pstTask->stTaskRdyLink.pstNext=&pstTask->stTaskRdyLink;
+	pstTask->stTaskRdyLink.pstPre=&pstTask->stTaskRdyLink;
+	
+	Task_vSetRdyTable(pstTask->u8Prio);
+	List_vAddNodeToFirst(&Task_apstTaskList[pstTask->u8Prio],&pstTask->stTaskRdyLink);
+	
+}
+
+void Task_vInitTaskRdyList(void)
+{
+	uint8 u8i;
+	for(u8i=0;u8i<RTOS_TASK_MAX_PRIO;u8i++)
+	{
+		Task_apstTaskList[u8i].u8NodeNum=0;
+		Task_apstTaskList[u8i].stHeadNode.pstNext=&Task_apstTaskList[u8i].stHeadNode;
+		Task_apstTaskList[u8i].stHeadNode.pstPre=&Task_apstTaskList[u8i].stHeadNode;
+	}
+}
